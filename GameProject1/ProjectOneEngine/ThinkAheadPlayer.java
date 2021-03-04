ccb3d44e8c2f3a79e5db941f721a11f22afa2b0a
kaihall <49812013+kaihall@users.noreply.github.com>
2021-03-04 16:44:14 -0500

    ThinkAheadPlayer: Infinite Loop Version
    Now runs but gets stuck in an infinite loop where it hops between the last level of the tree and the second to last level.

--- GameProject1/ProjectOneEngine/ThinkAheadPlayer.java
+++ GameProject1/ProjectOneEngine/ThinkAheadPlayer.java
@@ -9,12 +9,14 @@ public class ThinkAheadPlayer implements Player {
         public float value;
         public ArrayList<ValuedMove> children;
         public ValuedMove parent;
+        public boolean processed;
 
         public ValuedMove(Move move, float value) {
             this.move = move;
             this.value = value;
             this.children = new ArrayList<ValuedMove>();
             this.parent = null;
+            this.processed = false;
         }
 
         public void addChild(ValuedMove child) {
@@ -25,32 +27,35 @@ public class ThinkAheadPlayer implements Player {
 
     private MoveEvaluator moveEvaluator;
     private int depth = 10;
-    private PlayerID player;
+    private PlayerID maxPlayer;
+    private PlayerID minPlayer;
 
     public ThinkAheadPlayer(MoveEvaluator moveEvaluator) {
         this.moveEvaluator = moveEvaluator;
     }
 
     public Move getMove (GameState state) {
-        if (player == null) {
-            player = state.getCurPlayer();
+        if (maxPlayer == null) {
+            maxPlayer = state.getCurPlayer();
+            if (maxPlayer == PlayerID.TOP) minPlayer = PlayerID.BOT;
+            else minPlayer = PlayerID.BOT;
         }
-        return evaluateMoves(new ValuedMove(null, 0), state, depth=this.depth).move;
+        return evaluateMoves(new ValuedMove(null, 0), state, 0, this.depth).move;
     }
 
-    private ValuedMove evaluateMoves (ValuedMove curMove, GameState state, int n) {
+    private ValuedMove evaluateMoves (ValuedMove curMove, GameState state, int n, int depth) {
         int curDepth = n + 1;                           // Which level of the evaluation "tree" we're on right now
 
         if (curDepth > depth) return curMove;
 
-        // Get the possible moves, assign values to them, and store them in a list
         for (int i = 0; i < 6; i++){
             Move m = new Move(i, state.getCurPlayer());
             GameState gs = checkGameState(state, m);
             if (gs != null) {
+                System.out.println("New Move, depth: " + curDepth);
                 ValuedMove vm = new ValuedMove(m, 0);
                 curMove.addChild(vm);
-                ValuedMove newMove = evaluateMoves(vm, gs, curDepth);
+                ValuedMove newMove = evaluateMoves(vm, gs, curDepth, depth);
                 if (newMove == curMove) {
                     vm.value = moveEvaluator.evaluateMove(gs);
                 }
@@ -67,7 +72,7 @@ public class ThinkAheadPlayer implements Player {
 
     private ValuedMove getBestMove (List<ValuedMove> moveList, PlayerID moveMaker) {
         ValuedMove bestMove = new ValuedMove(new Move(1, moveMaker), -(Float.MAX_VALUE));    // Stores the best move
-        boolean myTurn = moveMaker == this.player;
+        boolean myTurn = moveMaker == this.maxPlayer;
         if (myTurn) bestMove.value *= -1;
 
         // Finds the move with the greatest value
