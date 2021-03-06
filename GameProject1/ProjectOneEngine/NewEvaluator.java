2289db0c989f59af09322b5a01da736428a8557f
Jacob Adkins <jacob.adkins18@ncf.edu>
2021-03-06 01:41:10 -0500

    New state evaluator to test

---
+++ GameProject1/ProjectOneEngine/NewEvaluator.java
@@ -0,0 +1,15 @@
+package ProjectOneEngine;
+
+public class NewEvaluator implements MoveEvaluator {
+
+    public float evaluateMove(GameState state){
+        if (state.getCurPlayer() == PlayerID.TOP){
+            System.out.println("I Think the value of this move is "+(state.top_home-state.bot_home));
+            return state.top_home - state.bot_home;
+        }
+        else{
+            System.out.println("I Think the value of this move is "+(state.bot_home-state.top_home));
+            return state.bot_home - state.top_home;
+        }
+    }
+}
