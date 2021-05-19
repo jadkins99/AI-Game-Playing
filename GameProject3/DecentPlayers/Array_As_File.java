fa487230e77354037871bf10ec067a326233bf4a
real-afluegge <32827316+real-afluegge@users.noreply.github.com>
2021-05-19 13:20:31 -0400

    saves 2d arrays to file

--- GameProject3/DecentPlayers/Array_As_File.java
+++ GameProject3/DecentPlayers/Array_As_File.java
@@ -8,47 +8,68 @@ import java.io.*;
 import java.util.ArrayList;
 
 public class Array_As_File {
-
     public static void main(String[] args){
         // the test array
-        ArrayList list = new ArrayList(); 
-        list.add("array value 1");
-        list.add(2222222);
-        list.add("array value 3");
+        int[][] list = {
+            {1,1,1},
+            {2,2,2},
+            {3,3,3}
+        };
 
         // function call to write to a new file
         to_file(list, "file_test");
 
         // function call to read from the file
-        from_file("file_test");
+        int[][] ret = from_file("file_test");
+        System.out.println(ret);
     }
 
     // to_file takes in an arraylist and the name of the file to write the array too
-    public static void to_file(ArrayList to_write, String file_name){
-        try{
-            FileOutputStream writeData = new FileOutputStream(file_name + ".ser");
-            ObjectOutputStream writeStream = new ObjectOutputStream(writeData);
+    public static void to_file(int[][] to_write, String file_name){
+
+        StringBuilder builder = new StringBuilder();
         
-            writeStream.writeObject(to_write);
-            writeStream.flush();
-            writeStream.close();
-        
-        }catch (IOException e) {
+        for(int i = 0; i < to_write.length; i++) {
+            for(int o = 0; o < to_write.length; o++) {
+                builder.append(to_write[i][o]+"");
+                if(o < to_write.length - 1) {
+                    builder.append(",");
+                }
+            }
+            builder.append("\n");
+        }
+
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(file_name + ".txt"));
+            writer.write(builder.toString());
+            writer.close();
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     // from_file takes the name of the file that the array is stored in
     // and returns an arraylist
-    public static ArrayList from_file(String file_name){
-        try{
-            FileInputStream readData = new FileInputStream(file_name + ".ser");
-            ObjectInputStream readStream = new ObjectInputStream(readData);
-        
-            ArrayList array = (ArrayList) readStream.readObject();
-            readStream.close();
-            System.out.println(array.toString());
-        }catch (Exception e) {
+    public static int[][] from_file(String file_name){
+        int[][] array = new int[3][3];
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(file_name + ".txt"));
+            String line = "";
+            int row = 0;
+
+            while((line = reader.readLine()) != null)
+            {
+                String[] cols = line.split(","); 
+                int col = 0;
+                for(String  c : cols) {
+                    array[row][col] = Integer.parseInt(c);
+                    col++;
+                }
+                row++;
+            }
+            reader.close();
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
